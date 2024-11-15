d4cab87ae63acbbaad92a1e37734b8b835ff093d
KeviinDavis <daviskevin1@hotmail.com>
2024-11-14 23:32:05 -0500

    Q3

--- solutions.js
+++ solutions.js
@@ -13,3 +13,16 @@ console.log(newNumbers);
 
 // Question 7
 
+
+
+//Question 9
+
+const result1 = 'bar' && 'foo';
+const result2 = false || 243;
+const result3 = 42 && false;
+// const result4 = myVar || 3000;
+
+console.log('result1:', result1);  // foo
+console.log('result2:', result2);  // 243
+console.log('result3:', result3);  // false
+// console.log('result4:', result4);  // 3000
